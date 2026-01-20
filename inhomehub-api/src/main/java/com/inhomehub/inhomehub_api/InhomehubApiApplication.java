6c72b5c439c9f4fb2fac7be89664ce386065c4a2
Alemão <270757@unifio.edu.br>
2026-01-20 19:55:09 -0300

    Initial commit for InHomeHub API project
    Add Spring Boot project structure for InHomeHub API, including Maven configuration, application entry point, basic test, and supporting files. Also includes project documentation.

---
+++ inhomehub-api/src/main/java/com/inhomehub/inhomehub_api/InhomehubApiApplication.java
@@ -0,0 +1,13 @@
+package com.inhomehub.inhomehub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhomehubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InhomehubApiApplication.class, args);
+	}
+
+}
