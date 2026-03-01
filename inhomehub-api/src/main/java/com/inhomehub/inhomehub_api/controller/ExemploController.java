aec681b72dff3a74f58318ffd49acf83f657eebe
Alemão <270757@unifio.edu.br>
2026-02-28 22:26:08 -0300

    Create ExemploController.java

---
+++ inhomehub-api/src/main/java/com/inhomehub/inhomehub_api/controller/ExemploController.java
@@ -0,0 +1,12 @@
+package com.inhomehub.inhomehub_api.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/exemplo")
+public class ExemploController {
+
+    private final
+}
